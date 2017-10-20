f9669411e48245b3981a40d6c1a2b031c32f4bcf
De-ma <dabuadas@uoguelph.ca>
2017-10-19 23:24:07 -0400

    initial commit/done UI

---
+++ VeCalcTests/VeCalcTests.swift
@@ -0,0 +1,36 @@
+//
+//  VeCalcTests.swift
+//  VeCalcTests
+//
+//  Created by Dema Abu Adas on 2017-10-19.
+//  Copyright © 2017 Dema Abu Adas. All rights reserved.
+//
+
+import XCTest
+@testable import VeCalc
+
+class VeCalcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
