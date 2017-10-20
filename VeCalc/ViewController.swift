1ca2e58d11416f6d73556ef11de3c97d06ac7f63
De-ma <dabuadas@uoguelph.ca>
2017-10-20 13:15:44 -0400

    added add functionality

--- VeCalc/ViewController.swift
+++ VeCalc/ViewController.swift
@@ -14,16 +14,27 @@ class ViewController: UIViewController {
     @IBOutlet weak var u1: UITextField!
     @IBOutlet weak var u2: UITextField!
     @IBOutlet weak var u3: UITextField!
+    @IBOutlet weak var v1: UITextField!
+    @IBOutlet weak var v2: UITextField!
+    @IBOutlet weak var v3: UITextField!
     
     /* Operations using 2 vectors */
     @IBOutlet weak var addBtn: UIButton!
+    @IBOutlet weak var subtractBtn: UIButton!
+    @IBOutlet weak var crossBtn: UIButton!
+    @IBOutlet weak var dotBtn: UIButton!
     
     /* Operations using 1 vector */
     @IBOutlet weak var magnitudeBtn: UIButton!
+    @IBOutlet weak var unitBtn: UIButton!
+    @IBOutlet weak var derivativeBtn: UIButton!
+    @IBOutlet weak var integral: UIButton!
     
     /* Result Vectors */
     @IBOutlet weak var singleResult: UILabel!
     @IBOutlet weak var xResult: UILabel!
+    @IBOutlet weak var yResult: UILabel!
+    @IBOutlet weak var zResult: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,8 +52,9 @@ class ViewController: UIViewController {
     @IBAction func buttonClicked(_ sender: UIButton) {
         //change this later when i learn how to do button "listeners" individually
         if (sender === addBtn) {
+            calcAdd()
         } else if (sender == magnitudeBtn) {
-            calcMagnitude();
+            calcMagnitude()
         }
     }
     
@@ -71,7 +83,16 @@ class ViewController: UIViewController {
         singleResult.text = finalString
 
     }
-
+    
+    func calcAdd() {
+        let addx: Double = Double(u1.text!)! + Double(v1.text!)!
+        let addy: Double = Double(u2.text!)! + Double(v2.text!)!
+        let addz: Double = Double(u3.text!)! + Double(v3.text!)!
+        
+        xResult.text = "\(addx)"
+        yResult.text = "\(addy)"
+        zResult.text = "\(addz)"
+    }
 
 }
 
